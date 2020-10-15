fb6858bb552c865cd49209fcea1ec63626fc17b3
digicope <51534708+digicope@users.noreply.github.com>
2020-10-15 11:52:35 +0900

    Add files via upload

--- 05_Word2Vec/mynlp.py
+++ 05_Word2Vec/mynlp.py
@@ -421,6 +421,37 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
 
 # # In[ ]:
 
+# contexts와 target을 구하는 함수
+def create_contexts_target(corpus,window_size=1):
+    target = corpus[window_size:-window_size]  
+
+    contexts = []
+    for idx in(range(window_size,len(corpus) -window_size)): 
+        cs = []
+        for t in range(-window_size,window_size+1) : 
+            if t == 0: 
+                continue
+            cs.append(corpus[idx + t]) 
+        contexts.append(cs)
+    return np.array(contexts),np.array(target) 
 
 
 
+# 원핫 변환 함수
+
+def convert_one_hot(corpus, vocab_size): 
+    N = corpus.shape[0] 
+
+    if corpus.ndim == 1: # target , 1차원인경우 ==> 2차원으로 출력
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32) # 0으로 초기화된  2차원 배열 생성 
+        for idx, word_id in enumerate(corpus): # 6회 반복
+            one_hot[idx, word_id] = 1  # 
+
+    elif corpus.ndim == 2: # contexts 2차원 인경우 ==> 3차원으로 출력
+        C = corpus.shape[1] # 
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32) # 0으로 초기화된 ) 3차원 배열 생성 
+        for idx_0, word_ids in enumerate(corpus): # 
+            for idx_1, word_id in enumerate(word_ids): #  
+                one_hot[idx_0, idx_1, word_id] = 1  
+
+    return one_hot
--- 05_Word2Vec/nn_layers.py
+++ 05_Word2Vec/nn_layers.py
@@ -317,5 +317,104 @@ class Adam:
             
             params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7)
             
-# https://dalpo0814.tistory.com/29
+
+import time
+import matplotlib.pyplot as plt
+plt.rc('font', family='Malgun Gothic')
+
+def remove_duplicate(params, grads):
+    '''
+    매개변수의 중복 제거 함수
+    매개변수 배열 중 중복되는 가중치를 하나로 모아
+    그 가중치에 대응하는 기울기를 더한다.
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 가중치 공유 시  : lSTM에서 사용
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 경사를 더함
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 가중치를 전치행렬로 공유하는 경우(weight tying)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
+
+
+class Trainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_list = []
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def fit(self, x, t, max_epoch=10, batch_size=32, max_grad=None, eval_interval=20):
+        data_size = len(x)
+        max_iters = data_size // batch_size
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            # 뒤섞기
+            idx = np.random.permutation(np.arange(data_size))
+            x = x[idx]
+            t = t[idx]
+
+            for iters in range(max_iters):
+                batch_x = x[iters*batch_size:(iters+1)*batch_size]
+                batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+                # 기울기 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    avg_loss = total_loss / loss_count
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 손실 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    self.loss_list.append(float(avg_loss))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = np.arange(len(self.loss_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.loss_list, label='train')
+        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('손실')
+        plt.show()      
+
+            
 
