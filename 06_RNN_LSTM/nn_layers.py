3bef1cea120531947ab03fd8af51e0f4955c3826
digicope <51534708+digicope@users.noreply.github.com>
2020-10-21 15:20:01 +0900

    Add files via upload

--- 06_RNN_LSTM/nn_layers.py
+++ 06_RNN_LSTM/nn_layers.py
@@ -584,8 +584,255 @@ class NegativeSamplingLoss:
             dscore = l0.backward(dout)   # SigmoidWithLoss 계층
             dh += l1.backward(dscore)   # EmbeddingDot 계층   
         return dh
+
+    
+# 기울기 클리핑 함수    
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:  # L2 norm 구하기 , 제곱의 합의 제곱근
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    # print('rate:',rate)
+    if rate < 1:     # total_norm 이 한계값(max_norm) 보다 클경우
+        for grad in grads:
+            grad *= rate
+            
+
+            
+# TimeEmbedding 클래스
+class TimeEmbedding:
+    def __init__(self,W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.layers = None
+        self.W = W
+    
+    def forward(self,xs):  # N : batch size, T : sequence length
+        N,T = xs.shape
+        V,D = self.W.shape  # V : Hidden size, 사용되지 않음  , D : RNN input size
+        
+        out = np.empty((N,T,D),dtype='f')   # W:(V,D) -> xs:(N,T) ==> (N,T,D)
+        self.layers = []
+        
+        for t in range(T):
+            layer = Embedding(self.W)
+            out[:,t,:] = layer.forward(xs[:,t])
+            self.layers.append(layer)
+            
+        return out  # RNN 계층으로 입력, (N,T,D) 3차원  
+    
+    def backward(self, dout) :
+        N, T, D = dout.shape
+        
+        grad = 0
+        for t in range(T):
+            layer = self.layers[t]
+            layer.backward(dout[:,t,:])
+            grad += layer.grads[0]
+        
+        self.grads[0][...] = grad
+        return None              
+
+    
+# TimeAffine 계층
+class TimeAffine:
+    def __init__(self,W,b):
+        self.params = [W,b]
+        self.grads = [np.zeros_like(W),np.zeros_like(b)]
+        self.x = None
+        
+    def forward(self,x):
+        N,T,D = x.shape   # N : batch size, T : sequence length , D : Affine input size,RNN의 출력사이즈
+        W,b = self.params
+        
+        rx = x.reshape(N*T,-1)  # 2차원으로 변경하여 내적 곱
+        out = np.dot(rx,W) + b
+        self.x = x
+        
+        return out.reshape(N,T,-1) # 3차원으로 변경하여 출력
+
+    def backward(self, dout):  # dout : 3차원
+        x = self.x
+        N, T, D = x.shape
+        W, b = self.params
+        
+        dout = dout.reshape(N*T, -1)  #  2차원으로 변경
+        rx = x.reshape(N*T,-1)        #  2차원으로 변경
+        
+        db = np.sum(dout,axis=0)
+        dW = np.dot(rx.T, dout)
+        dx = np.dot(dout,W.T)
+        dx = dx.reshape(*x.shape)  # 튜플 가변인수,(N,T,D)
+        
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        
+        return dx        
+    
+
+
+# TimeSoftmaxWithLoss 계층
+class TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+        self.ignore_label = -1
+
+    def softmax(self,x):
+        if x.ndim == 2:
+            x = x - x.max(axis=1, keepdims=True)
+            x = np.exp(x)
+            x /= x.sum(axis=1, keepdims=True)
+        elif x.ndim == 1:
+            x = x - np.max(x)
+            x = np.exp(x) / np.sum(np.exp(x))
+
+        return x        
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+
+        if ts.ndim == 3:  # 정답 레이블이 원핫 벡터인 경우
+            ts = ts.argmax(axis=2)
+
+        mask = (ts != self.ignore_label)  # label 이 -1이 아닌 값만 True 가  되는 2차원 배열을 생성
+
+        # 배치용과 시계열용을 정리(reshape)
+        xs = xs.reshape(N * T, V)   # 2차원으로
+        ts = ts.reshape(N * T)      # 1차원으로
+        mask = mask.reshape(N * T)  # 1차원으로 
+
+        ys = self.softmax(xs)
+        ls = np.log(ys[np.arange(N * T), ts])
+        ls *= mask                # ignore_label(-1)에 해당하는 데이터는 손실을 0으로 설정
+        loss = -np.sum(ls)        # T개의 loss를 합한다
+        loss /= mask.sum()        # 평균
+
+        self.cache = (ts, ys, mask, (N, T, V))
+        return loss
+
+    def backward(self, dout=1):
+        ts, ys, mask, (N, T, V) = self.cache
+
+        dx = ys        # softmax의 출력 값과
+        dx[np.arange(N * T), ts] -= 1
+        dx *= dout
+        dx /= mask.sum()
+        dx *= mask[:, np.newaxis]  # ignore_label(-1)에 해당하는 데이터는 기울기를 0으로 설정, mask를 2차원으로 변경하여 곱해줌
+
+        dx = dx.reshape((N, T, V)) # 다시 3차원으로 변경
+
+        return dx
     
     
-    
+# RNNLM의 Trainer 클래스
+import time
+
+class RnnlmTrainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.time_idx = None
+        self.ppl_list = None
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def get_batch(self, x, t, batch_size, time_size):
+        batch_x = np.empty((batch_size, time_size), dtype='i')
+        batch_t = np.empty((batch_size, time_size), dtype='i')
+
+        data_size = len(x)
+        jump = data_size // batch_size
+        offsets = [i * jump for i in range(batch_size)]  # 배치에서 각 샘플을 읽기 시작하는 위치
+
+        for time in range(time_size):
+            for i, offset in enumerate(offsets):
+                batch_x[i, time] = x[(offset + self.time_idx) % data_size]
+                batch_t[i, time] = t[(offset + self.time_idx) % data_size]
+            self.time_idx += 1
+        return batch_x, batch_t
+
+    def fit(self, xs, ts, max_epoch=10, batch_size=20, time_size=35,
+            max_grad=None, eval_interval=20):
+        data_size = len(xs)
+        max_iters = data_size // (batch_size * time_size)
+        self.time_idx = 0
+        self.ppl_list = []
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            for iters in range(max_iters):
+                batch_x, batch_t = self.get_batch(xs, ts, batch_size, time_size)
+
+                # 기울기를 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 퍼플렉서티 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    ppl = np.exp(total_loss / loss_count)
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 퍼플렉서티 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, ppl))
+                    self.ppl_list.append(float(ppl))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = np.arange(len(self.ppl_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.ppl_list, label='train')
+        plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('퍼플렉서티')
+        plt.show()
+
+def remove_duplicate(params, grads):
+    '''
+    매개변수 배열 중 중복되는 가중치를 하나로 모아
+    그 가중치에 대응하는 기울기를 더한다.
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 가중치 공유 시
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 경사를 더함
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 가중치를 전치행렬로 공유하는 경우(weight tying)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
+            
     
     
