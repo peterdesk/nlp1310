aab7bd03998b5d312ba5d653f00b31417ebc572a
digicope <51534708+digicope@users.noreply.github.com>
2020-10-27 13:47:34 +0900

    Add files via upload

--- 06_RNN_LSTM/nn_layers.py
+++ 06_RNN_LSTM/nn_layers.py
@@ -1273,4 +1273,45 @@ class Seq2seq:
         
         return sampled    
     
+ # generate()을 호출하여 예측한 답이 맞는지 여부를 출력하고 맞으면 1을 틀리면 0을 반환해주는  함수
+# Q 77+85  
+# T 162 
+# O 162 
+def eval_seq2seq(model, question, correct, id_to_char,
+                 verbos=False, is_reverse=False):
+    correct = correct.flatten()
+    # 머릿글자
+    start_id = correct[0]
+    correct = correct[1:]
+    guess = model.generate(question, start_id, len(correct))
+
+    # 문자열로 변환
+    question = ''.join([id_to_char[int(c)] for c in question.flatten()])
+    correct = ''.join([id_to_char[int(c)] for c in correct])
+    guess = ''.join([id_to_char[int(c)] for c in guess])
+
+    if verbos:
+        if is_reverse:
+            question = question[::-1]
+
+        colors = {'ok': '\033[92m', 'fail': '\033[91m', 'close': '\033[0m'}
+        print('Q', question)
+        print('T', correct)
+
+        is_windows = os.name == 'nt'
+
+        if correct == guess:
+            mark = colors['ok'] + '☑' + colors['close']
+            if is_windows:
+                mark = 'O'
+            print(mark + ' ' + guess)
+        else:
+            mark = colors['fail'] + '☒' + colors['close']
+            if is_windows:
+                mark = 'X'
+            print(mark + ' ' + guess)
+        print('---')
+
+    return 1 if guess == correct else 0
+        
     
